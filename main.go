fd80f8f357e4528fb61484578c4bd9fff2301cfd
Luca Morgado <luca.morgado.pro@gmail.com>
2024-02-20 09:09:59 +0100

    added iptables command to open port 80

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"api_runner/handlers"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"os/exec"
 )
 
 func main() {
+
+	// open port 80
+	cmd := exec.Command("iptables", "-A", "INPUT", "-p", "tcp", "--dport", "80", "-j", "ACCEPT")
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
 	router := gin.Default()
 	router.GET("/vm", handlers.GetAllContainersFromVM)
 	router.GET("/vm/:id/containers", handlers.GetContainersFromVM)
@@ -14,6 +22,6 @@ func main() {
 	router.POST("/build", handlers.BuildImage)
 	router.DELETE("/vm/:id", handlers.DeleteContainer)
 
-	fmt.Println("Server is running on localhost:8080")
-	router.Run("localhost:8080")
+	fmt.Println("Server is running on localhost:80")
+	router.Run("localhost:80")
 }
